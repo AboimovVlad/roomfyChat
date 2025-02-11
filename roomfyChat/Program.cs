feec884a1faf3acde04241e3603fd47bc68eb223
ЕвроКомп <@DESKTOP-9VED43L>
2025-02-11 18:00:11 +0200

    Добавьте файлы проекта.

---
+++ roomfyChat/Program.cs
@@ -0,0 +1,51 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace roomfyChat
+{
+    class Program
+    {
+        internal static void Main(string[] args)
+        {
+            var client = new TelegramBotClient("8068346833:AAGLjXMkf6BJ3lMannkocLTasMTGIOCiy8M");
+            client.StartReceiving(update,error);
+            Console.WriteLine("Bot Starting");
+
+            Console.ReadLine();
+        }
+
+        private static async Task update(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+
+            if(message?.Text != null)
+            {
+                Console.WriteLine($"user: {message.Chat.Id} {message.Chat.FirstName ?? "No Name"} : {message.Text}");
+
+                if (message.Text.ToLower().Contains("start"))
+                {
+                    await botClient.SendMessage(message.Chat.Id, "Привіт👋\nЯ Roomfy Chat і я готовий вам допомгти знайти нових друзів");
+
+                    var keyBord = new ReplyKeyboardMarkup(new[]
+                    {
+                        new KeyboardButton[] {"Так!"}
+                    })
+                    {
+                        ResizeKeyboard = true,
+                        OneTimeKeyboard = false
+                    };
+
+                    await botClient.SendMessage(message.Chat.Id, "Готовий розпочати реєстрацію?", replyMarkup: keyBord);
+                }
+            }
+        }
+
+        private static async Task error(ITelegramBotClient client, Exception exception, HandleErrorSource source, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
